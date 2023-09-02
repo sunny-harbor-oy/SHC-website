990f94da3bdd6a0558d63641b542ef10693ebcb9
Leon Gustafsson <gustafsson.leon1@gmail.com>
2023-09-02 13:23:15 +0300

    added webp pictures

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import blackRock from "../assets/planet.png";
 import capsuleImg from "../assets/capsule.png";
 import AboutPage from "../pages/about.jsx";
 import ContactPage from "../pages/contact.jsx";
-import PartnersPage from "../pages/partners.jsx";
 import SHCFooter from "../elements/shcFooter";
 
 const motto = [
@@ -117,8 +115,8 @@ export default function HomePage(language = "fi") {
           muted
           loop
           id="backgroundVideo"
-          /* className="w-full object-cover z-0 top-0 m-0 p-0 bgVideoFilter" */
-          className="w-full object-cover z-0 top-0 m-0 p-0"
+          /* className="top-0 z-0 object-cover w-full p-0 m-0 bgVideoFilter" */
+          className="top-0 z-0 object-cover w-full p-0 m-0"
         >
           {/* <source src="./src/assets/HomePageBackground.mp4" type="video/mp4" /> */}
           <source src="./src/assets/earth.mp4" type="video/mp4" />
@@ -127,37 +125,37 @@ export default function HomePage(language = "fi") {
           id="content"
           className="absolute flex flex-col justify-center items-center text-white h-[50vh] md:h-screen"
         >
-          <h1 className="font-bold text-3xl lg:text-6xl text-center select-none cursor-default text-h1">
+          <h1 className="text-3xl font-bold text-center cursor-default select-none lg:text-6xl text-h1">
             Sunny Harbor Consulting
           </h1>
           <p
             id="titleDesc"
-            className="font-normal font-mono sm:text-2xl lg:text-3xl text-center text-p select-none cursor-default"
+            className="font-mono font-normal text-center cursor-default select-none sm:text-2xl lg:text-3xl text-p"
           ></p>
-          <div className="text-2xl font-mono flex w-full mt-8 justify-around lg:visible hidden">
+          <div className="flex justify-around hidden w-full mt-8 font-mono text-2xl lg:visible">
             <button 
             onClick={() => document.getElementById('aboutUsWrapper').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })}
-            className="bg-black px-2  text-white text-center">Tiimi</button>
+            className="px-2 text-center text-white bg-black">Tiimi</button>
             <button 
             onClick={() => document.getElementById('contactWrapper').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })}
-            className="bg-black px-2 text-white text-center">Ota yhteyttä</button>
+            className="px-2 text-center text-white bg-black">Ota yhteyttä</button>
             <button 
             onClick={() => document.getElementById('Partners').scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })}
-            className="bg-black px-2  text-white text-center">Kumppanit</button>
+            className="px-2 text-center text-white bg-black">Kumppanit</button>
           </div>
         </div>
       </div>
-      <div id="content" className="bg-white mx-0">
-        <div id="about" className="w-full m-0 p-0 block h-screen">
+      <div id="content" className="mx-0 bg-white">
+        <div id="about" className="block w-full h-screen p-0 m-0">
           <div className="xl:h-screen h-1/2 z-[0]">
             <div
               id="storyDiv"
               ref={sectionOne}
-              className="relative bg-black grid grid-cols-1 text-h2 p-5 pt-5 lg:p-32 w-full m-auto xl:h-screen h-full"
+              className="relative grid w-full h-full grid-cols-1 p-5 pt-5 m-auto bg-black text-h2 lg:p-32 xl:h-screen"
             >
               <div className="relative overflow-hidden w-full z-[10]">
-              <h1 className="xl:text-7xl text-4xl font-raleway">Sunny Harbor Consulting</h1>
-              <h2 className="lg:w-2/3 md:w-2/3 w-full text-h1 text-base sm:text-2xl lg:text-5xl 2xl:text-5xl font-mono mt-7 relative">
+              <h1 className="text-4xl xl:text-7xl font-raleway">Sunny Harbor Consulting</h1>
+              <h2 className="relative w-full font-mono text-base lg:w-2/3 md:w-2/3 text-h1 sm:text-2xl lg:text-5xl 2xl:text-5xl mt-7">
                   Sunny Harbor Consulting eli SHC on nuori ja taitava ohjelmistoyritys,
                   joka erikoistuu innovatiivisiin web- ja mobiilisovelluksiin.
 <br/><br/>
@@ -165,47 +163,47 @@ export default function HomePage(language = "fi") {
                 SHC tekee mobiili- ja tietokonesovelluksia, nettisivuja sekä embedded-toteutuksia. Osaamisemme vain laajenee tästä ja panostamme uuden osaamisen hankkimiseen.
                 </h2>
               </div>
-              <img className="h-4/5 absolute aspect-square xl:right-5 right-0 z-[0] xl:bottom-[8%] bottom-[0]" src={"https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/as4-01-580.jpg"} />
+              <img className="h-4/5 absolute aspect-square xl:right-5 right-0 z-[0] xl:bottom-[8%] bottom-[0]" src={".src/assets/earth.webp"} />
             </div>
           </div>
           <div className="xl:h-screen h-1/2 z-[0]">
             <div
               id="storyDiv"
               ref={sectionSecond}
-              className="relative bg-black grid grid-cols-1 text-h2 p-5 pt-5 lg:p-32 w-full m-auto h-screen"
+              className="relative grid w-full h-screen grid-cols-1 p-5 pt-5 m-auto bg-black text-h2 lg:p-32"
             >
               <div className="inline-block align-middle overflow-hidden w-full z-[10]">
-                <h1 className="xl:text-7xl text-4xl font-raleway">Nuori mutta kokenut</h1>
-                <h2 className="lg:w-2/3 md:w-2/3 w-full text-h1 text-base sm:text-2xl lg:text-5xl 2xl:text-5xl font-mono mt-7 relative">
+                <h1 className="text-4xl xl:text-7xl font-raleway">Nuori mutta kokenut</h1>
+                <h2 className="relative w-full font-mono text-base lg:w-2/3 md:w-2/3 text-h1 sm:text-2xl lg:text-5xl 2xl:text-5xl mt-7">
                 Meillä on kokemusta seuraavien ohjelmointikielien kanssa työskentelystä: C, C++, Python, Golang, JavaScript, TypeScript, Dart, Java, Kotlin, Swift, Lua, Rust, Mojo, Assembly (x86, ARM ja Z80) ja muilla.
                 </h2>
               </div>
-              <img src={capsuleImg} className="absolute h-full left-0 top-0 grayscale object-cover" />
+              <img src={capsuleImg} className="absolute top-0 left-0 object-cover h-full grayscale" />
             </div>
           </div>
           <AboutPage />
           <div className="pt-10 pb-10 md:py-32">
             {" "}
-            <h1 className="md:pb-10 pb-5 font-lora md:text-7xl text-4xl text-center text-black bg-white">
+            <h1 className="pb-5 text-4xl text-center text-black bg-white md:pb-10 font-lora md:text-7xl">
               Yhteistyökumppanit
             </h1>
             <div
               id="Partners"
-              className="bg-white flex md:justify-between justify-around md:w-1/2 ml-auto mr-auto"
+              className="flex justify-around ml-auto mr-auto bg-white md:justify-between md:w-1/2"
             >
-              <Link to="/partners/sykekotipalvelut" className="md:w-2/5 w-1/3">
+              <Link to="/partners/sykekotipalvelut" className="w-1/3 md:w-2/5">
                 <img
-                  className="w-full object-contain hover:cursor-pointer"
+                  className="object-contain w-full hover:cursor-pointer"
                   alt="Partner Image"
                   src="https://www.sykekotipalvelut.fi/wp-content/uploads/2020/11/SYKE_UUSI-LOGO-02.png"
                 ></img>
               </Link>
               <Link
                 to="/partners/korjausvelkalaskuri"
-                className="md:w-2/5 w-1/3"
+                className="w-1/3 md:w-2/5"
               >
                 <img
-                  className="w-full object-contain hover:cursor-pointer"
+                  className="object-contain w-full hover:cursor-pointer"
                   alt="Partner Image"
                   src="https://korjausvelkalaskuri.fi/wp-content/uploads/2023/01/korjausvelkalaskuri-207x60px.png.webp"
                 ></img>
