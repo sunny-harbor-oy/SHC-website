a678e6b0d645f1e5b5ee3a8a031961dc3c550690
Leon Gustafsson <gustafsson.leon1@gmail.com>
2023-09-04 22:06:26 +0300

    fixed video maybe

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -25,35 +25,42 @@ let navBarY = null;
 let done = false;
 let isMobile = window.innerWidth < 821;
 
-window.addEventListener("scroll", () => {
-  if (sectionOne && sectionSecond && !isMobile) {
-    const scrollPos = window.scrollY;
+window.addEventListener(
+  "scroll",
+  () => {
+    if (sectionOne && sectionSecond && !isMobile) {
+      const scrollPos = window.scrollY;
 
-    if (scrollPos + navBarY < oneRect.y) {
-      done = false;
-      sectionOne.current.style.position = "relative";
-    }
-    else if (scrollPos + navBarY > oneRect.y && (scrollPos < secondRect.bottom-navBarY) && !done) {
-      sectionOne.current.style.position = "fixed";
-      sectionOne.current.style.top = `${0}px`;
+      if (scrollPos + navBarY < oneRect.y) {
+        done = false;
+        sectionOne.current.style.position = "relative";
+      } else if (
+        scrollPos + navBarY > oneRect.y &&
+        scrollPos < secondRect.bottom - navBarY &&
+        !done
+      ) {
+        sectionOne.current.style.position = "fixed";
+        sectionOne.current.style.top = `${0}px`;
 
-      if (scrollPos > secondRect.y) {
-        sectionSecond.current.style.position = "fixed";
-        sectionSecond.current.style.top = `${0}px`;
-      } else {
+        if (scrollPos > secondRect.y) {
+          sectionSecond.current.style.position = "fixed";
+          sectionSecond.current.style.top = `${0}px`;
+        } else {
+          sectionSecond.current.style.position = "relative";
+          sectionSecond.current.style.top = `${0}px`;
+        }
+      } else if (!done) {
+        sectionOne.current.style.position = "relative";
+        sectionOne.current.style.top = `${0}px`;
+
         sectionSecond.current.style.position = "relative";
         sectionSecond.current.style.top = `${0}px`;
+        done = true;
       }
-    } else if (!done) {
-      sectionOne.current.style.position = "relative";
-      sectionOne.current.style.top = `${0}px`;
-
-      sectionSecond.current.style.position = "relative";
-      sectionSecond.current.style.top = `${0}px`;
-      done = true;
     }
-  }
-}, []);
+  },
+  []
+);
 
 export default function HomePage(language = "fi") {
   sectionOne = useRef(null);
@@ -105,24 +112,33 @@ export default function HomePage(language = "fi") {
     }, 60);
 
     setTimeout(() => {
-      navBarY = document.getElementById("navBarWrapper").getBoundingClientRect().y;
+      navBarY = document
+        .getElementById("navBarWrapper")
+        .getBoundingClientRect().y;
     }, 1000);
   }, []);
   return (
     <div id="homeWrapper" className="bg-main">
       <meta http-equiv="ScreenOrientation" content="autoRotate:disabled"></meta>
-      <div id="main" className="h-[50vh] flex justify-center mt-0 md:h-screen">
-        <video
-          autoPlay
-          muted
-          loop
-          id="backgroundVideo"
-          /* className="top-0 z-0 object-cover w-full p-0 m-0 bgVideoFilter" */
-          className="top-0 z-0 object-cover w-full p-0 m-0"
-        >
-          {/* <source src="./src/assets/HomePageBackground.mp4" type="video/mp4" /> */}
-          <source src={earthVideo} type="video/mp4" />
-        </video>
+      <div
+        id="main"
+        className="h-[50vh] flex justify-center mt-0 md:h-screen"
+        dangerouslySetInnerHTML={{
+          __html: (
+            <video
+              autoPlay
+              muted
+              loop
+              id="backgroundVideo"
+              /* className="top-0 z-0 object-cover w-full p-0 m-0 bgVideoFilter" */
+              className="top-0 z-0 object-cover w-full p-0 m-0"
+            >
+              {/* <source src="./src/assets/HomePageBackground.mp4" type="video/mp4" /> */}
+              <source src={earthVideo} type="video/mp4" />
+            </video>
+          ),
+        }}
+      >
         <div
           id="content"
           className="absolute flex flex-col justify-center items-center text-white h-[50vh] md:h-screen"
@@ -135,15 +151,48 @@ export default function HomePage(language = "fi") {
             className="font-normal text-center cursor-default select-none font-raleway sm:text-2xl lg:text-3xl text-p"
           ></p>
           <div className="flex justify-around hidden w-full mt-8 font-mono text-2xl lg:visible">
-            <button 
-            onClick={() => document.getElementById('aboutUsWrapper').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })}
-            className="px-2 text-center text-white bg-black">Tiimi</button>
-            <button 
-            onClick={() => document.getElementById('contactWrapper').scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })}
-            className="px-2 text-center text-white bg-black">Ota yhteyttä</button>
-            <button 
-            onClick={() => document.getElementById('Partners').scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })}
-            className="px-2 text-center text-white bg-black">Kumppanit</button>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("aboutUsWrapper")
+                  .scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "nearest",
+                  })
+              }
+              className="px-2 text-center text-white bg-black"
+            >
+              Tiimi
+            </button>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("contactWrapper")
+                  .scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "nearest",
+                  })
+              }
+              className="px-2 text-center text-white bg-black"
+            >
+              Ota yhteyttä
+            </button>
+            <button
+              onClick={() =>
+                document
+                  .getElementById("Partners")
+                  .scrollIntoView({
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "nearest",
+                  })
+              }
+              className="px-2 text-center text-white bg-black"
+            >
+              Kumppanit
+            </button>
           </div>
         </div>
       </div>
@@ -156,16 +205,25 @@ export default function HomePage(language = "fi") {
               className="relative grid w-full h-full grid-cols-1 p-5 pt-5 m-auto bg-black text-h2 lg:p-32 xl:h-screen"
             >
               <div className="relative overflow-hidden w-full z-[10]">
-              <h1 className="text-4xl italic font-bold xl:text-7xl font-poppins">Sunny Harbor Consulting</h1>
-              <h2 className="relative w-full mt-2 text-base font-poppins font-extralight lg:w-2/3 md:w-2/3 text-h1 sm:text-2xl md:text-3xl lg:text-5xl 2xl:text-5xl">
-                  Sunny Harbor Consulting eli SHC on nuori ja taitava ohjelmistoyritys,
-                  joka erikoistuu innovatiivisiin web- ja mobiilisovelluksiin.
-                <br/><br/>
-                <br/>
-                SHC tekee mobiili- ja tietokonesovelluksia, nettisivuja sekä embedded-toteutuksia. Osaamisemme vain laajenee tästä ja panostamme uuden osaamisen hankkimiseen.
+                <h1 className="text-4xl italic font-bold xl:text-7xl font-poppins">
+                  Sunny Harbor Consulting
+                </h1>
+                <h2 className="relative w-full mt-2 text-base font-poppins font-extralight lg:w-2/3 md:w-2/3 text-h1 sm:text-2xl md:text-3xl lg:text-5xl 2xl:text-5xl">
+                  Sunny Harbor Consulting eli SHC on nuori ja taitava
+                  ohjelmistoyritys, joka erikoistuu innovatiivisiin web- ja
+                  mobiilisovelluksiin.
+                  <br />
+                  <br />
+                  <br />
+                  SHC tekee mobiili- ja tietokonesovelluksia, nettisivuja sekä
+                  embedded-toteutuksia. Osaamisemme vain laajenee tästä ja
+                  panostamme uuden osaamisen hankkimiseen.
                 </h2>
               </div>
-              <img className="h-4/5 absolute aspect-square xl:right-5 right-0 z-[0] xl:bottom-[8%] bottom-[0]" src={earthImg} />
+              <img
+                className="h-4/5 absolute aspect-square xl:right-5 right-0 z-[0] xl:bottom-[8%] bottom-[0]"
+                src={earthImg}
+              />
             </div>
           </div>
           <div className="xl:h-screen h-1/2 z-[0]">
@@ -175,13 +233,22 @@ export default function HomePage(language = "fi") {
               className="relative grid w-full h-screen grid-cols-1 p-5 pt-5 m-auto bg-black text-h2 lg:p-32"
             >
               <div className="inline-block align-middle overflow-hidden w-full z-[10]">
-                <h1 className="text-4xl xl:text-7xl font-poppins">Mitä osaamme?</h1>
+                <h1 className="text-4xl xl:text-7xl font-poppins">
+                  Mitä osaamme?
+                </h1>
                 <h2 className="relative w-full mt-2 text-base font-poppins font-extralight lg:w-2/3 md:w-2/3 text-h1 sm:text-2xl md:text-3xl lg:text-5xl 2xl:text-5xl">
-                Olemme moniosaava ohjelmistokehitys yritys. Toteutamme teille mobiili-, netti- ja embedded-sovelluksia. <br/><br/>
-                Meillä on kokemusta seuraavien ohjelmointikielien kanssa: C, C++, Python, Golang, JavaScript, TypeScript, Dart, Java, Kotlin, Swift, Lua, Rust, Mojo ja Assembly (x86, ARM ja Z80).
+                  Olemme moniosaava ohjelmistokehitys yritys. Toteutamme teille
+                  mobiili-, netti- ja embedded-sovelluksia. <br />
+                  <br />
+                  Meillä on kokemusta seuraavien ohjelmointikielien kanssa: C,
+                  C++, Python, Golang, JavaScript, TypeScript, Dart, Java,
+                  Kotlin, Swift, Lua, Rust, Mojo ja Assembly (x86, ARM ja Z80).
                 </h2>
               </div>
-              <img src={capsuleImg} className="absolute top-0 left-0 object-cover w-full h-full grayscale" />
+              <img
+                src={capsuleImg}
+                className="absolute top-0 left-0 object-cover w-full h-full grayscale"
+              />
             </div>
           </div>
           <AboutPage />
