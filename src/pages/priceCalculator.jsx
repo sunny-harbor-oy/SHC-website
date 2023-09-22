df7a85427daf9bd9c3f97d8fe836186f5b99acc0
TurpoFIN <turpofin@gmail.com>
2023-09-23 00:04:01 +0300

    Progress on the price estimator.

--- src/pages/priceCalculator.jsx
+++ src/pages/priceCalculator.jsx
@@ -1,54 +1,77 @@
 import { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import {renderToString} from "react-dom/server";
 
 
 export default function PriceEstimation() {
-  const cardData = [
-    {
-      title: "Suuruusluokka",
-      question: "Minkä kokoinen projekti on",
-      options: {
-        "Pieni": {scale: 3},
-        "Keskikokoinen": {scale: 6},
-        "Suuri": {scale: 10},
-      },
+const cardData = [  
+{
+    title: "Suuruusluokka",
+    question: "Minkä kokoinen projekti on?",
+    answers: {
+    "Pieni": {scale: 3},
+    "Keskikokoinen": {scale: 6},
+    "Suuri": {scale: 10},
     },
-    {
-      title: "Alusta",
-      question: "Millä alustoilla ohjelmiston pitäisi toimia?",
-      options: {
-        "Selain": {complexity: 5},
-        "Mobiili": {complexity: 8},
-        "Tietokone": {complexity: 10},
-      },
+},
+{
+    title: "Alusta",
+    question: "Millä alustoilla ohjelmiston pitäisi toimia?",
+    settings: {
+        multipleChoice: true,
     },
-  ];
+    answers: {
+    "Selain": {complexity: 5},
+    "Mobiili": {complexity: 8},
+    "Tietokone": {complexity: 10},
+    },
+},
+{
+    title: "Kuinka yksilöidyn ohjelmiston haluat?",
+    question: "Kuinka yksilöidyn ohjelmiston haluat?",
+    answers: {
+    "Valmis ohjelmisto": {complexity: 1, scale: 1, cost: 1},
+    "Valmis ohjelmisto, jossa pieniä muutoksia": {complexity: 3, scale: 2, cost: 2},
+    "Valmis ohjelmisto, jossa suuria muutoksia": {complexity: 5, scale: 3, cost: 4},
+    "Ohjelmisto, joka on tehty alusta asti sinulle": {complexity: 10, scale: 5, cost: 6},
+    }
+},
+];
 
 
 const progressBar = cardData.map((card) => {
-    return <div className="opacity-100 transition-all duration-200"></div>;
+    return <div className="h-[0.75vw] w-full rounded-lg transition-all duration-[250ms]"></div>;
 });
 
 let chosenOptions = [];
 let currentCard = 0;
+let currentCardObject = undefined;
 let lastOptionSelected = undefined;
+let tranition = false;
+
 const slideDiv = useRef(null);
+const barDiv = useRef(null);
+const report = useRef(null);
 
 let cardId = -1;
 const FormElements = cardData.map((card) => {
     cardId++;
     let optionId = -1;
+    let getId = () => {
+        optionId++;
+        return optionId;
+    }
+
     return (
-        <div questionid={cardId} className="opacity-0 transition-all duration-200">
-            <h1 className="text-2xl font-bold">{card.title}</h1>
-            <div className="flex flex-col">
-                {Object.entries(card.options).map(([option, value]) => (
-                    <div optionid={optionId++ + ":" + cardId} className="" onClick={ () => {
-                            if (lastOptionSelected) lastOptionSelected.classList.remove("color-calculatorSelected");
-                            this.classList.add("color-calculatorSelected");
-                            chosenOptions.join(value);
-                        }
-                        }>
+        <div questionid={cardId} className="h-full transition-all duration-[500ms] w-full px-[4vw] pt-[3vw]">
+            <h1 className="text-[3vw] w-4/5 font-poppins font-extrabold">{card.title}</h1>
+            <div className="flex flex-col px-[2vw] py-[1vw]">
+                {Object.entries(card.answers).map(([option, value]) => (
+                    <div className="flex justify-between w-3/4 text-white my-[1vw] mx-[2vw] py-[0.75vw] px-[2vw] bg-[#2b2d42] rounded-lg">
+                        <h1 className="font-poppins text-[2vw]">
+                            {option}
+                        </h1>
+                        <div className="bg-white w-[1.25vw] h-[1.25vw] my-auto transition-all duration-[250ms] rounded-sm" />
                     </div>
                 ))}
             </div>
@@ -56,46 +79,154 @@ const FormElements = cardData.map((card) => {
     );
 });
 
+const mergeObjects = (obj1, obj2) => {
+    let merged = {};
+    Object.keys(obj1).forEach((key) => {
+        merged[key] = obj1[key];
+    });
+    Object.keys(obj2).forEach((key) => {
+        merged[key] = obj2[key];
+    });
+    return merged;
+}
+
+const decaultSettings = {
+    multipleChoice: false,
+}
+
+const getCardOption = (option, cardId = currentCard) => {
+    if (cardData[cardId] == undefined) return undefined;
+
+    const card = cardData[cardId];
+    const settings = card.settings == undefined ? decaultSettings : mergeObjects(decaultSettings, card.settings);
+
+    return settings[option] ? settings[option] : false;
+}
+
 const finalPrice = () => {
     return (
         <div>
-            
+            <h1 className="text-center font-poppins text-[6vw] font-extrabold">Onneksi olkoon!</h1>
+            <h2 className="text-center font-poppins text-[3vw]">1000000€</h2>
         </div>
     );
 }
 
-const nextCard = () => {
-    slideDiv.current.innerHTML = "";
+const updateStages = () => {
+    let stages = barDiv.current.children;
+    for (let i = 0; i < stages.length; i++) {
+        if (i < currentCard) {
+            stages[i].style["background-color"] = "#1eb82a";
+        } else if (i == currentCard) {
+            stages[i].style["background-color"] = "#ffd366";
+        }
+        else {
+            stages[i].style["background-color"] = "#c6d0d8";
+        }
+    }
+}
 
-    if (currentCard == cardData.length) {
-        finalPrice();
-        return;
+const chooseOption = (optionValues) => {
+    if (chosenOptions[currentCard] == undefined) chosenOptions[currentCard] = {};
+    let currentValues = chosenOptions[currentCard];
+
+    if (getCardOption("multipleChoice") == false && lastOptionSelected != undefined) {
+        if (lastOptionSelected.questionId != optionValues.questionId) {
+            lastOptionSelected.self.children[1].style["background-color"] = "#c6d0d8";
+            currentValues[`${lastOptionSelected.questionId}`] = undefined;
+        }
+
+        lastOptionSelected = undefined;
     }
 
-    console.log(slideDiv.current);
+    if (currentValues[`${optionValues.questionId}`] == optionValues.value) {
+        currentValues[`${optionValues.questionId}`] = undefined;
+        chosenOptions[currentCard] = currentValues;
 
-    let test = FormElements[currentCard];
+        return false;
+    }
+    else {
+        currentValues[`${optionValues.questionId}`] = optionValues.value;
+        chosenOptions[currentCard] = currentValues;
 
-    ReactDOM.render(test, slideDiv.current, () => {
-        const firstChild = slideDiv.current.firstChild;
-        console.log(firstChild);  
-        firstChild.style["opacity"] = "100";
-        currentCard++;
-    })
+        return true;
+    }
+}
 
+const renderCard = (cardId) => {
+    if (FormElements[cardId] == undefined) return;
+
+    slideDiv.current.innerHTML = "";
+    lastOptionSelected = undefined;
+
+    let card = FormElements[cardId];
+    currentCardObject = cardData[cardId];
+
+    slideDiv.current.innerHTML = renderToString(card);
+
+    const answers = slideDiv.current.firstChild.children[1].children;
+
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].addEventListener("click", () => {
+            const self = answers[i];
+            const ownId = i;
+
+            const answerValue = Object.values(cardData[cardId].answers)[i];
+            const state = chooseOption({questionId: ownId, value: answerValue});
+
+            self.children[1].style["background-color"] = state ? "#1eb82a" : "#c6d0d8";
+            lastOptionSelected = state ? {questionId: ownId, self} : undefined;
+        });        
+    }
+
+    updateStages();
+}
+
+const changeCard = (change) => {
+    if (tranition) return;
+
+    currentCard += change;
+
+    if (currentCard < 0) currentCard = 0;
+
+    if (currentCard == cardData.length) {
+        updateStages();
+
+        slideDiv.current.parentElement.style.display = "none";
+
+        report.current.innerHTML = renderToString(finalPrice());
+        report.current.style.display = "block";
+
+        return;
+    } else {
+        renderCard(currentCard);
+        slideDiv.current.parentElement.style.display = "flex";
+        report.current.style.display = "none";
+    }
+    
 }
 
 useEffect(() => {
-    nextCard();
+    changeCard(0);
+    window.addEventListener("load", changeCard(0));
+    return () => {
+        window.removeEventListener("load", changeCard(0));
+    }
 });
 
 return (
-    <div className="h-full w-full">
-        <div className="w-1/2">
-            {progressBar}
+    <div className="h-screen max-h-screen overflow-hidden w-[75vw] pt-32 mx-auto">
+        <div className="w-1/2 mx-auto pt-[4vw]">
+            <div className={`grid gap-4 mx-auto w-full`} style={{ placeItems: 'center', gridTemplateColumns: `repeat(${cardData.length < 10 ? cardData.length : 10}, minmax(0, 1fr))`}} ref={barDiv}>
+                {progressBar}
+            </div>
         </div>
-        <div ref={slideDiv}>
-
+        <div className="flex w-[70vw] justify-between mx-auto h-[40vw]]">
+            <button onClick={() => changeCard(-1)}><i className="fa fa-angle-left"></i></button>
+            <div ref={slideDiv} className="w-[70vw]"></div>
+            <button onClick={() => changeCard(1)}><i className="fa fa-angle-right"></i></button>
+        </div>
+        <div ref={report} className="w-[70vw] h-[40vw] mx-auto py-[4vw]">
         </div>
     </div>
 );
