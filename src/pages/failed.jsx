4677087cc55dfdb30f5b535d6defb322b3650149
TurpoFIN <turpofin@gmail.com>
2024-01-10 15:32:34 +0200

    -

---
+++ src/pages/failed.jsx
@@ -0,0 +1,10 @@
+export default function FailedPage() {
+    return (
+        <div className="h-screen w-screen bg-primary font-poppins">
+            <div className="relative top-[45%] translate-y-[-50%]">
+            <h1 className="text-secondary md:text-[2vw] font-semibold text-center">Yhteydenotto epäonnistui.</h1>
+            <h2 className="text-white md:text-[1.25vw] text-center">Pahoittelemme epäonnistuneesta yrityksestä ja<br/>toivomme, että kokeilet uudestaan.</h2>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/thanks.jsx
@@ -0,0 +1,10 @@
+export default function OkPage() {
+    return (
+        <div className="h-screen w-screen bg-primary font-poppins">
+            <div className="relative top-[45%] translate-y-[-50%]">
+            <h1 className="text-secondary md:text-[2vw] font-semibold text-center">Kiitos yhteydenotosta!</h1>
+            <h2 className="text-white md:text-[1.5vw] text-center">Vastaamme mahdollisimman pian.</h2>
+            </div>
+        </div>
+    )
+}
