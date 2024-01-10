5f65f705deca7a3cc760242b92e8cd1beb6ceb6c
TurpoFIN <turpofin@gmail.com>
2024-01-10 17:16:42 +0200

    -

--- src/pages/failed.jsx
+++ src/pages/failed.jsx
@@ -2,8 +2,8 @@ export default function FailedPage() {
     return (
         <div className="h-screen w-screen bg-primary font-poppins">
             <div className="relative top-[45%] translate-y-[-50%]">
-            <h1 className="text-secondary md:text-[2vw] font-semibold text-center">Yhteydenotto epäonnistui.</h1>
-            <h2 className="text-white md:text-[1.25vw] text-center">Pahoittelemme epäonnistuneesta yrityksestä ja<br/>toivomme, että kokeilet uudestaan.</h2>
+            <h1 className="text-secondary md:text-[2vw] text-[6vw] font-semibold text-center">Yhteydenotto epäonnistui.</h1>
+            <h2 className="text-white md:text-[1.25vw] text-[3.75vw] text-center">Pahoittelemme epäonnistuneesta yrityksestä.<br/>Kokeile uudestaan.</h2>
             </div>
         </div>
     )
