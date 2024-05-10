d6ae27134c3df7877eab285a85e140a69dda0fb7
erikssonsisu <sisu.eriksson@gmail.com>
2024-05-10 11:34:23 +0300

    SEO

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,9 +17,6 @@ export default function HomePage(language = "fi") {
     <div id="homeWrapper" className="w-screen overflow-x-hidden bg-primary">
       <Helmet>
         <title>Sunny Harbor</title>
-        <title>Sunny Harbor Consulting</title>
-        <title>sunnyharbor</title>
-        <title>Sunny Harbour</title>
         <meta
           name="description"
           content="Tavoitteemme on olla alan läpinäkyvin ja luotettavin vaihtoehto IT-ratkaisuillesi! Taidokkaasti suunnittelemamme mobiili- ja verkkosovellukset on tarkoitettu juuri sinulle, jos tavoitteesi on luoda yrityksellesi ainutlaatuinen kilpailuetu."
