7cd1a471fab56245ebcebb1f1f0a9129d9ab6eb1
erikssonsisu <sisu.eriksson@gmail.com>
2024-05-10 11:13:57 +0300

    Google SEO

--- src/pages/blogs/shc_automatization.jsx
+++ src/pages/blogs/shc_automatization.jsx
@@ -11,23 +11,36 @@ export default function AutomatizationBlog() {
       id="blog"
       className="bg-white pt-[8vh] md:pt-[18vh] md:pb-[10vh] w-screen"
     >
-       <Helmet>
-        <title>Milloin henkilöstönhallinta kannattaa tehdä ohjelmiston avulla?</title>
+      <Helmet>
+        <title>
+          Milloin henkilöstönhallinta kannattaa tehdä ohjelmiston avulla?
+        </title>
         <meta
           name="description"
           content="Tarpeellisuus työvuorojen jakamisen automatisoinnissa korostuu erityisesti suurissa organisaatioissa, missä manuaalinen koordinointi aiheuttaa tehottomuutta ja virheitä. Työvuorojen jakaminen ohjelmiston avulla tarjoaa tehokkaan tavan koordinoida eri työtehtäviä organisaatiossa. Lisäksi se mahdollistaa joustavan työvuorojen hallinnan ja parantaa työn tehokkuutta."
         />
-        <meta property="og:title" content="Milloin henkilöstönhallinta kannattaa tehdä ohjelmiston avulla?" />
+        <meta
+          property="og:title"
+          content="Milloin henkilöstönhallinta kannattaa tehdä ohjelmiston avulla?"
+        />
         <meta
           property="og:description"
           content="Tarpeellisuus työvuorojen jakamisen automatisoinnissa korostuu erityisesti suurissa organisaatioissa, missä manuaalinen koordinointi aiheuttaa tehottomuutta ja virheitä. Työvuorojen jakaminen ohjelmiston avulla tarjoaa tehokkaan tavan koordinoida eri työtehtäviä organisaatiossa. Lisäksi se mahdollistaa joustavan työvuorojen hallinnan ja parantaa työn tehokkuutta."
         />
-          <meta name="keywords" content="henkilöstönhallinta, ohjelmiston avulla, työvuorojen jakaminen, automatisointi, organisaatio, työntekijät, työtehtävät, ohjelmisto, työvuorot, koordinointi, viestintä, haastavuus, työntekijöiden välillä, inhimilliset virheet, kommunikaatio" />
-
-          <meta property="og:image" content={office} />
-          <meta property="og:url" content="https://sunnyharbor.fi/blogs/shc_automatization" />
-          <meta name="author" content="Victor Ocampo" />
-          <link rel="canonical" href="https://sunnyharbor.fi/blogs/shc_automatization" />
+        <meta property="og:image" content={office} />
+        <meta
+          name="keywords"
+          content="henkilöstönhallinta, ohjelmiston avulla, työvuorojen jakaminen, automatisointi, organisaatio, työntekijät, työtehtävät, ohjelmisto, työvuorot, koordinointi, viestintä, haastavuus, työntekijöiden välillä, inhimilliset virheet, kommunikaatio"
+        />
+        <meta
+          property="og:url"
+          content="https://sunnyharbor.fi/blogs/shc_automatization"
+        />
+        <meta name="author" content="Victor Ocampo" />
+        <link
+          rel="canonical"
+          href="https://sunnyharbor.fi/blogs/shc_automatization"
+        />
       </Helmet>
       <div className="w-[100vw] md:w-[80vw] mx-auto max-w-[900px] md:mt-[1v] py-[4vh] px-[4vh] rounded-lg bg-white">
         <Title>
@@ -60,35 +73,40 @@ export default function AutomatizationBlog() {
           ikuisuudeksi. Kuitenkin tällaisen ohjelmiston hankinta on merkittävä
           kuluerä. Jos on vaikka vain 5 työntekijää niin kallis
           ohjelmistoratkaisu ei ole välttämättä sen arvoinen.
-   
         </SubParagraph>
         <SubParagraph>
-         <h1 className="w-full text-[1.25rem] md:text-[1.2rem] mt-0 md:leading-[1.4rem] text-black font-roboto">
-          Tässä on neljä kriteeriä, minkä perusteella voidaan päättää hankinnan
-          tarpeellisuus: </h1> <br /> <br />
+          <h1 className="w-full text-[1.25rem] md:text-[1.2rem] mt-0 md:leading-[1.4rem] text-black font-roboto">
+            Tässä on neljä kriteeriä, minkä perusteella voidaan päättää
+            hankinnan tarpeellisuus:{" "}
+          </h1>{" "}
+          <br /> <br />
           <span className="font-bold">
             {" "}
             1. Organisaatiolla on usein tarve jakaa työvuoroja eri
             työntekijöille.{" "}
           </span>
           Mitä enemmän työvuoroja on jaettava, sitä tarpeellisempi
-          automatisointi on. <br />  <br />
+          automatisointi on. <br /> <br />
           <span className="font-bold">
             2. Työvuorojen jako vaatii liikaa viestintää.{" "}
           </span>
           Mikäli käytössä on jokin suuri työntekijöiden Whatsapp-ryhmä tai
           pahimmillaan työvuoroja jaetaan yksityisviestien avulla pienikin
           työvuorojen määrä voi varastaa joltain monta tuntia. <br /> <br />
-          <span className="font-bold">3. Eri työntekijöiden välillä koordinointi on haastavaa. </span>Joissain
-          organisaatioissa työntekijöille annetaan ohjeet heti aamusta
+          <span className="font-bold">
+            3. Eri työntekijöiden välillä koordinointi on haastavaa.{" "}
+          </span>
+          Joissain organisaatioissa työntekijöille annetaan ohjeet heti aamusta
           toimipisteellä eri työtehtävistä. Kun kyse on kuitenkin sellaisesta
           tilanteessa, jossa työvuoroja/-tehtäviä ei jaella heti aamusta
           toimipisteellä vaan jokainen työntekijä toimii lähtökohtaisesti
           itsenäisesti ja ottaa vastaan työvuoroja itsenäisesti niin
           työntekijöiden koordinointi alkaa olemaan erittäin haastavaa. Kun
           haastavuus on riittävä, ohjelmiston hankkiminen tätä koordinointia
-          varten voi olla hyvä suunnitelma. <br />  <br />
-          <span className="font-bold">4. Inhimilliset virheet näkyvät työvuorojen jaossa ja vastaanotossa. </span>
+          varten voi olla hyvä suunnitelma. <br /> <br />
+          <span className="font-bold">
+            4. Inhimilliset virheet näkyvät työvuorojen jaossa ja vastaanotossa.{" "}
+          </span>
           Inhimillisiä virheitä tulee joka työssä ja varsinkin kun kyse on
           kommunikaatiosta. Inhimillisten virheiden määrä kääntyy selvästi
           laskuun, kun otetaan käyttöön ohjelmisto, jossa työvuorojen hallinta
